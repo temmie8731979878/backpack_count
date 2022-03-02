f37d11aea63d5c717666d38550190bd6b08d8b62
temmie8731979878 <kapoo950807@gmail.com>
2022-03-02 22:04:14 +0800

    add progress

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var load = function(){
-    let l = [0, 1, 6, 10, 662]; // red, purple, yellow, white, gray
+    let l = [0, 1, 6, 10, 663]; // red, purple, yellow, white, gray
 
     document.getElementById("red").innerHTML = "自主存儲單位數量: " + l[0].toString();
     document.getElementById("purple").innerHTML = "公事包數量: " + l[1].toString();
@@ -7,6 +7,7 @@ var load = function(){
     document.getElementById("white").innerHTML = "腰包數量: " + l[3].toString();
     document.getElementById("gray").innerHTML = "老舊袋子數量: " + l[4].toString();
     document.getElementById("need").innerHTML = "還需要的老舊袋子數量: " + (75000 - l[0]*75000 - l[1]*3000 - l[2]*150 - l[3]*10 - l[4]).toString();
+    document.getElementById("progress_bar").innerHTML = '<div class="percent">' + Math.floor((l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4])/75000*100).toString() + '%</div><progress value="' + (l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4]).toString() + '" max="75000" class="progress_bar" id="progress_bar"></progress>'
 }
 
 load();
