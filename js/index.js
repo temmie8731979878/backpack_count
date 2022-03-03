b03b4b7bdb04f6918c0a715541c28469abcc54c9
temmie8731979878 <kapoo950807@gmail.com>
2022-03-03 23:23:04 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var load = function(){
     let l = [0, 1, 10, 10, 1165]; // red, purple, yellow, white, gray
 
+    
     document.getElementById("red").innerHTML = "自主存儲單位數量: " + l[0].toString();
     document.getElementById("purple").innerHTML = "公事包數量: " + l[1].toString();
     document.getElementById("yellow").innerHTML = "探險家背包數量: " + l[2].toString();
