cb94c9d905696f2d69235ccaa0a85e69ed0f00fe
temmie8731979878 <kapoo950807@gmail.com>
2022-03-06 21:56:50 +0800

    -

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var load = function(){
-    let l = [0, 1, 10, 24, 3354]; // red, purple, yellow, white, gray
+    let l = [0, 1, 10, 24, 3366]; // red, purple, yellow, white, gray
 
     document.getElementById("red").innerHTML = "自主存儲單位數量: " + l[0].toString();
     document.getElementById("purple").innerHTML = "公事包數量: " + l[1].toString();
@@ -7,7 +7,8 @@ var load = function(){
     document.getElementById("white").innerHTML = "腰包數量: " + l[3].toString();
     document.getElementById("gray").innerHTML = "老舊袋子數量: " + l[4].toString();
     document.getElementById("need").innerHTML = "還需要的老舊袋子數量: " + (75000 - l[0]*75000 - l[1]*3000 - l[2]*150 - l[3]*10 - l[4]).toString();
-    document.getElementById("progress_bar").innerHTML = '<div class="percent">' + Math.floor((l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4])/75000*100).toString() + '%</div><progress value="' + (l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4]).toString() + '" max="75000" class="progress_bar" id="progress_bar"></progress>'
+    document.getElementById("progress_bar_1").innerHTML = '<div class="percent">' + Math.floor((l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4])/75000*100).toString() + '%</div><progress value="' + (l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4]).toString() + '" max="75000" class="progress_bar_1" id="progress_bar_1"></progress>'
+    document.getElementById("progress_bar_2").innerHTML = '<div class="percent">' + Math.floor((l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4])%750/750*100).toString() + '%</div><progress value="' + ((l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4])%750).toString() + '" max="750" class="progress_bar_2" id="progress_bar_2"></progress>'
 }
 
 load();
