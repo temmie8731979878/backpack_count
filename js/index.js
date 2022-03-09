75012cbad1afdda5030ca8fb57acf93cf705213a
temmie8731979878 <kapoo950807@gmail.com>
2022-03-09 17:26:42 +0800

    -

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var load = function(){
-    let l = [0, 4, 44, 87, 6646]; // red, purple, yellow, white, gray
+    let l = [0, 4, 45, 87, 8446]; // red, purple, yellow, white, gray
 
     document.getElementById("red").innerHTML = "自主存儲單位數量: " + l[0].toString();
     document.getElementById("purple").innerHTML = "公事包數量: " + l[1].toString();
