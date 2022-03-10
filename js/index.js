b0dc1809e9e5d2755fa6c5e5d52fc90c74e2f3ad
temmie8731979878 <kapoo950807@gmail.com>
2022-03-10 20:52:21 +0800

    -

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var load = function(){
-    let l = [0, 4, 45, 121, 8940]; // red, purple, yellow, white, gray
+    let l = [0, 4, 45, 121, 9447]; // red, purple, yellow, white, gray
 
     document.getElementById("red").innerHTML = `自主存儲單位數量: ${l[0]}`;
     document.getElementById("purple").innerHTML = `公事包數量: ${l[1]}`;
@@ -9,10 +9,15 @@ var load = function(){
     document.getElementById("need").innerHTML = `還需要的老舊袋子數量: ${75000 - l[0]*75000 - l[1]*3000 - l[2]*150 - l[3]*10 - l[4]}`;
 
     var max = Math.floor((l[0]*75000 + l[1]*3000 + l[2]*150 + l[3]*10 + l[4])/75000*10000)/100;
-    document.getElementById("progress_bar_1").innerHTML = `
-    <div class="percent">${max}%</div>
-    <progress value="${max*750}" max="75000" class="progress_bar_1" id="progress_bar_1"></progress>;
+    document.getElementById("progress_container_1").innerHTML = `
+    <div class="progress_text" id="progress_text">${max}%</div>
     `
+    document.getElementById("progress_container_2").innerHTML = `
+    <div class="progress_bg" id="progress_bg">.</div>
+    <div class="progress_v" id="progress_v" style="--pixel: ${max/100*600}px">.</div>
+    <div class="progress_point" id="progress_point"></div>
+    `
+    console.log(max/100*600)
 }
 
 load();
