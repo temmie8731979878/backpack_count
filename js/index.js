507f93c3cbc4ea41b60f8496ca0549b55c4f3964
temmie8731979878 <kapoo950807@gmail.com>
2022-03-13 20:30:32 +0800

    -

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var load = function(){
-    let l = [0, 4, 48, 150, 13338]; // red, purple, yellow, white, gray
+    let l = [0, 4, 48, 150, 13475]; // red, purple, yellow, white, gray
 
     document.getElementById("red").innerHTML = `自主存儲單位數量: ${l[0]}`;
     document.getElementById("purple").innerHTML = `公事包數量: ${l[1]}`;
